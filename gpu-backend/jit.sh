e3120d9102067af72b561278a73e524c222be1c4
Niansong Zhang <nz264@cornell.edu>
2022-08-22 10:26:46 -0400

    update script to llvm15

--- gpu-backend/jit.sh
+++ gpu-backend/jit.sh
@@ -1,19 +1,20 @@
-export LLVM_INSTALL_DIR=/work/shared/users/phd/nz264/llvm-project-14.0.0
+export LLVM_INSTALL_DIR=/work/shared/common/llvm-project-15.0.0-gpu
 export LD_LIBRARY_PATH=$LLVM_INSTALL_DIR/build/lib:$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH=/work/shared/common/usr/local/lib:/work/shared/common/usr/local/lib64:$LD_LIBRARY_PATH
 export PATH=$LLVM_INSTALL_DIR/build/bin/:$PATH
 
-mlir-opt matmul.mlir \
-    --convert-linalg-to-parallel-loops \
-    --test-gpu-greedy-parallel-loop-mapping \
-    --convert-parallel-loops-to-gpu \
-    --gpu-kernel-outlining \
-    --lower-affine \
-    --convert-scf-to-std \
-    --canonicalize \
-    --pass-pipeline="gpu.module(strip-debuginfo,convert-gpu-to-nvvm,gpu-to-cubin)" --gpu-to-llvm \
-| mlir-cpu-runner \
-    --shared-libs=$LLVM_INSTALL_DIR/build/lib/libmlir_cuda_runtime.so \
-    --shared-libs=$LLVM_INSTALL_DIR/build/lib/libmlir_runner_utils.so \
-    --shared-libs=$LLVM_INSTALL_DIR/build/lib/libmlir_c_runner_utils.so \
-    --entry-point-result=void
+which mlir-opt
+mlir-opt lowered_linalg.mlir \
+      --convert-linalg-to-parallel-loops 
+#     --test-gpu-greedy-parallel-loop-mapping 
+#     --convert-parallel-loops-to-gpu \
+#     --gpu-kernel-outlining \
+#     --lower-affine \
+#     --convert-scf-to-std \
+#     --canonicalize \
+#     --pass-pipeline="gpu.module(strip-debuginfo,convert-gpu-to-nvvm,gpu-to-cubin)" --gpu-to-llvm \
+# mlir-opt matmul_llvm.mlir | mlir-cpu-runner \
+#     --shared-libs=$LLVM_INSTALL_DIR/build/lib/libmlir_cuda_runtime.so \
+#     --shared-libs=$LLVM_INSTALL_DIR/build/lib/libmlir_runner_utils.so \
+#     --shared-libs=$LLVM_INSTALL_DIR/build/lib/libmlir_c_runner_utils.so \
+#     --entry-point-result=void
